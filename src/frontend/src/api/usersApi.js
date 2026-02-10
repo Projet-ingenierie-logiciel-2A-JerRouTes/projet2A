42ebcd0aeef03bee5b8fcb88a333b20f6b0fa20d
MaximeRoux-ENSAI <maxime.roux@eleve.ensai.fr>
2026-02-11 00:15:24 +0100

    ✨ feat(frontend): ajout d’un module usersApi pour les actions utilisateur

---
+++ src/frontend/src/api/usersApi.js
@@ -0,0 +1,19 @@
+import API from "./apiClient";
+
+export async function me() {
+  const res = await API.get("/api/users/me");
+  return res.data; // typiquement { user: {...} }
+}
+
+export async function updateMe({ username, email }) {
+  const res = await API.patch("/api/users/me", { username, email });
+  return res.data;
+}
+
+export async function changePassword({ old_password, new_password }) {
+  const res = await API.post("/api/users/me/change-password", {
+    old_password,
+    new_password,
+  });
+  return res.data;
+}
